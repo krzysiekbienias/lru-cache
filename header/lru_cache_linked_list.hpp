1c54cc3b60c5d27c1237c7446812c099838e292b
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-05-05 13:04:37 +0200

    refactor: simplify linked-list cache declarations with aliases

---
+++ header/lru_cache_linked_list.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include<unordered_map>
+#include <list>
+
+class LRUCache {
+public:
+    LRUCache(int capacity);
+    int get(int key);
+    void put(int key, int value);
+
+private:
+    int m_capacity;
+    using CacheEntry = std::pair<int, int>;
+    using ListIt = std::list<CacheEntry>::iterator;
+
+    std::list<CacheEntry> lruList;
+    std::unordered_map<int, ListIt> cacheMap;
+
+};
